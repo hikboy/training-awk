3ae04569ef9293c8460cd9fe4a7166b2814dacd7
ljb <hikboy@outlook.com>
2019-04-08 20:20:15 +0800

    added new commands and test-files

---
+++ awk-101.sh
@@ -0,0 +1,9 @@
+#This file is like sed
+
+#awk -Fs '/pattern/ {action}' input-file
+#awk -Fs '{action}' input-file
+#awk -Fs -f myscript.awk input-file
+#awk -f myscript.awk input-file
+
+
+#awk -F: '/mail/ {print $1}' $1
